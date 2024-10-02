b04d884d5180540f028bf71f79e4f149aae6964f
Sergei Kornev <kornev.se@intechs.by>
2024-10-02 10:23:34 +0300

    fix/clear-array

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -2,9 +2,6 @@ import child_process from 'child_process';
 
 const SCRIPT_FILENAME = './src/cp/files/script.js';
 
-const dataArray = [];
-
-
 const spawnChildProcess = async (args) => {
     child_process.fork(SCRIPT_FILENAME, args);
 };
